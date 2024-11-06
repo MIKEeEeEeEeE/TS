c6723030824b00c56c8c6a64983a7856ff75b30b
mike <kuzmish02@gmail.com>
2024-11-07 00:40:39 +0100

    🐱

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import { drizzle } from 'drizzle-orm/pglite';
+import express, { Request, Response } from 'express';
+import * as dotenv from "dotenv";
+import * as pg from 'drizzle-orm/pg-core';
+
+
+const db = drizzle();
 console.log('hi');
